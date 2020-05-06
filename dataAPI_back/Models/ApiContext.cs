abad54afa48ec0e228dbcb92df44a356dabb6d97
simaoppcastro <a31716216@fam.ulusiada.pt>
2020-05-06 23:05:47 +0100

    front-end commit

---
+++ dataAPI_back/Models/ApiContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataAPI.Models
+{
+    public class ApiContext : DbContext
+    {
+        // constructor
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options) { }
+
+        // 3 tables -> for each entety model
+        public DbSet<Client> Clients { get; set; }
+        public DbSet<Orders> Orders { get; set; }
+        public DbSet<Server> Servers { get; set; }
+    }
+}
---
+++ dataAPI_back/Models/Orders.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DataAPI.Models
+{
+    public class Orders
+    {
+        public int Id { get; set; }
+        public Client Client { get; set; }
+        public decimal Total { get; set; }
+        public DateTime Placed { get; set; }
+        public DateTime? Completed { get; set; }
+    }
+}
