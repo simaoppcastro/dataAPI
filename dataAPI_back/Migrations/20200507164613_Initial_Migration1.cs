fc9bba8a5318b6e7bfada960af7b2c0b9e3c2ebf
simaoppcastro <a31716216@fam.ulusiada.pt>
2020-05-07 17:47:02 +0100

    initial migration

---
+++ dataAPI_back/Migrations/20200507164613_Initial_Migration1.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dataAPI_back.Migrations
+{
+    public partial class Initial_Migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
