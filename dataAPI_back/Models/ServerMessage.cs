b117962cae9eed41e640103b7070fd2d4a4fce99
simaoppcastro <a31716216@fam.ulusiada.pt>
2020-06-01 21:53:22 +0100

    ServerController and ServerMessage 010620

---
+++ dataAPI_back/Models/ServerMessage.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace dataAPI_back.Models
+{
+    public class ServerMessage
+    {
+        public int Id { get; set; }
+        public string Payload { get; set; }
+        // DateTime Sent { get; set; }
+    }
+}
